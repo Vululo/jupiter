bf8909d1043769d56f38debc4844a6bcb302d6c3
am11DAM2 <am11DAM2@informatica.iesjc>
2024-10-07 13:43:07 +0200

    home.css add

--- holaMundo.java
+++ holaMundo.java
@@ -1,5 +1,8 @@
 public class holaMundo{
 	public static void main(String [] args){
-		system.out.println("hello world");
+		System.out.println("blucle");
+		while(true){
+			system.out.println("hello world");
+		}	
 	}
 }
